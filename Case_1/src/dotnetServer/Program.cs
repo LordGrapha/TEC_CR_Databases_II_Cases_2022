fbe8e42a971c07fdb5015a76f088172bc273fcd1
LuisDiegoMora98 <aguilarluisdi@estudiantec.cr>
2022-02-10 00:24:51 -0600

    Directory Structure + Setup programs for Java + C# + Typescript

---
+++ Case_1/src/dotnetServer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace dotnetServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Case 1 - Aseni for you and our democracy World!");
+            Console.WriteLine("Author: Luis Diego Mora Aguilar, 2018147110");
+        }
+    }
+}
---
+++ Case_1/src/typescriptServer/app.ts
@@ -0,0 +1,19 @@
+class app {
+   
+    constructor() {
+        this.launch();
+    }
+   
+    /*
+    functName(param : type) {
+        
+    }
+    */
+
+    launch(){
+        console.log("Case 1 - Aseni for you and our democracy World!");
+        console.log("Author: Luis Diego Mora Aguilar, 2018147110");
+    }
+}
+
+const sv = new app();
