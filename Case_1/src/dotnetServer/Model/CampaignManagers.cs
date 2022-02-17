ec63093714919ad25cba9f9f017091b056cac8d0
LuisDiegoMora98 <aguilarluisdi@estudiantec.cr>
2022-02-17 08:04:16 -0600

    SQL Connection + Models ready for ORM + SELECT tested

---
+++ Case_1/src/dotnetServer/Model/CampaignManagers.cs
@@ -0,0 +1,20 @@
+class CampaignManagers
+{
+    private int id;
+    private string bio;
+    private string pictureUrl;
+    private int politicPartyId;
+
+    public int Id
+    { get; set; }
+
+    public string Bio
+    { get; set; }
+
+    public string PictureUrl
+    { get; set; }
+
+    public string PoliticPartyId
+    { get; set; }
+
+}
