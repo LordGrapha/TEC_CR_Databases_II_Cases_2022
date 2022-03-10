01aed6e5930c28f3d0b15ecfb2fbc492b3ece72c
LuisDiegoMora98 <aguilarluisdi@estudiantec.cr>
2022-03-09 23:50:25 -0600

    sql Script Full with tables and test data. Still need to implement Spacial Data in Cantons and Citizens test data, assigned NULL meanwhile

--- Case_2/sql_Script/database.sql
+++ Case_2/sql_Script/database.sql
@@ -2,25 +2,26 @@
 IF NOT EXISTS ( SELECT name FROM master.dbo.sysdatabases 
                 WHERE name = N'Case_2' )
 BEGIN
-    CREATE DATABASE [Case_2];
+    CREATE DATABASE Case_2;
 END
 GO
 
 USE [Case_2];
 GO
 
+-- FK constraint format name is FK_targetTableName_sourceTableName
+
 IF OBJECT_ID(N'dbo.PoliticParties', N'U') IS NULL
 BEGIN
     CREATE TABLE PoliticParties (
 		politicpartyid INT IDENTITY(1,1) PRIMARY KEY,
 		name VARCHAR(63) NOT NULL,
-		flagPictureUrl VARCHAR(255) NOT NULL,
+		flagpictureurl VARCHAR(255) NOT NULL,
 		posttime DATE NOT NULL DEFAULT GETDATE(),
 		deleted BIT NOT NULL DEFAULT 0,
 		enabled BIT NOT NULL DEFAULT 1
 	);
 END
-GO
 
 IF OBJECT_ID(N'dbo.Users', N'U') IS NULL
 BEGIN
@@ -35,7 +36,6 @@ BEGIN
 		enabled BIT NOT NULL DEFAULT 1
 	);
 END
-GO
 
 IF OBJECT_ID(N'dbo.Roles', N'U') IS NULL
 BEGIN
@@ -47,7 +47,6 @@ BEGIN
 		enabled BIT NOT NULL DEFAULT 1
 	);
 END
-GO
 
 IF OBJECT_ID(N'dbo.UsersXRoles', N'U') IS NULL
 BEGIN
@@ -60,7 +59,20 @@ BEGIN
 		FOREIGN KEY (userid) REFERENCES Users(userid)
 	);
 END
-GO
+
+IF OBJECT_ID(N'dbo.Cantons', N'U') IS NULL
+BEGIN
+	CREATE TABLE Cantons (
+		cantonid INT IDENTITY(1,1) PRIMARY KEY,
+		name VARCHAR(63) NOT NULL,
+		cantonlocation GEOGRAPHY DEFAULT NULL,
+		politicPartiesSupport INT NOT NULL DEFAULT 0,
+		deliverables INT NOT NULL DEFAULT 0,
+		posttime DATE NOT NULL DEFAULT GETDATE(),
+		deleted BIT NOT NULL DEFAULT 0,
+		enabled BIT NOT NULL DEFAULT 1
+	);
+END
 
 IF OBJECT_ID(N'dbo.Citizens', N'U') IS NULL
 BEGIN
@@ -68,10 +80,22 @@ BEGIN
 		citizenid INT IDENTITY(1,1) PRIMARY KEY,
 		userid INT NOT NULL,
 		CONSTRAINT FK_users_citizens
-		FOREIGN KEY (userid) REFERENCES Users(userid)
+		FOREIGN KEY (userid) REFERENCES Users(userid),
+		citizenlocation GEOGRAPHY DEFAULT NULL
 	);
 END
-GO
+
+IF OBJECT_ID(N'dbo.CitizensXCantons', N'U') IS NULL
+BEGIN
+	CREATE TABLE CitizensXCantons (
+		citizenid INT NOT NULL,
+		CONSTRAINT FK_citizens_citizensxcantons
+		FOREIGN KEY (citizenid) REFERENCES Citizens(citizenid),
+		cantonid INT NOT NULL,
+		CONSTRAINT FK_cantons_citizensxcantons
+		FOREIGN KEY (cantonid) REFERENCES Cantons(cantonid),
+	);
+END
 
 IF OBJECT_ID(N'dbo.CampaignManagers', N'U') IS NULL
 BEGIN
@@ -87,13 +111,12 @@ BEGIN
 		FOREIGN KEY (userid) REFERENCES Users(userid)
 	);
 END
-GO
 
 IF OBJECT_ID(N'dbo.Actions', N'U') IS NULL
 BEGIN
     CREATE TABLE Actions (
 		actionid INT IDENTITY(1,1) PRIMARY KEY,
-		politicpartyid INT NOT NULL
+		politicpartyid INT NOT NULL,
 		CONSTRAINT FK_politicparties_actions
 		FOREIGN KEY (politicpartyid) REFERENCES PoliticParties(politicpartyid),
 		action VARCHAR(1022) NOT NULL,
@@ -102,7 +125,6 @@ BEGIN
 		enabled BIT NOT NULL DEFAULT 1
 	);
 END
-GO
 
 IF OBJECT_ID(N'dbo.Deliverables', N'U') IS NULL
 BEGIN
@@ -122,29 +144,18 @@ BEGIN
 		enabled BIT NOT NULL DEFAULT 1
 	);
 END
-GO
-
-IF OBJECT_ID(N'dbo.Cantons', N'U') IS NULL
-BEGIN
-	CREATE TABLE Cantons (
-		cantonid INT IDENTITY(1,1) PRIMARY KEY,
-		name VARCHAR(63) NOT NULL,
-		cantonlocation GEOGRAPHY DEFAULT NULL,
-		politicPartiesSupport INT NOT NULL DEFAULT 0,
-		deliverables INT NOT NULL DEFAULT 0
-	);
-END
-GO
 
 IF OBJECT_ID(N'dbo.KPIs', N'U') IS NULL
 BEGIN
 	CREATE TABLE KPIs (
 		kpiid INT IDENTITY(1,1) PRIMARY KEY,
 		kpivalue INT,
-		measurement VARCHAR(63)
+		measurement VARCHAR(63),
+		posttime DATE NOT NULL DEFAULT GETDATE(),
+		deleted BIT NOT NULL DEFAULT 0,
+		enabled BIT NOT NULL DEFAULT 1
 	);
 END
-GO
 
 IF OBJECT_ID(N'dbo.CantonsXDeliverables', N'U') IS NULL
 BEGIN
@@ -154,16 +165,17 @@ BEGIN
 		FOREIGN KEY (cantonid) REFERENCES Cantons(cantonid),
 		deliverableid INT NOT NULL,
 		CONSTRAINT FK_deliverables_cantonsxdeliverables
-		FOREIGN KEY (deliverableid) REFERENCES Cantons(deliverableid)
-		kpiValue INT NOT NULL,
-		kpiEntity VARCHAR(63) NOT NULL
+		FOREIGN KEY (deliverableid) REFERENCES Deliverables(deliverableid),
+		kpiid INT NOT NULL,
+		CONSTRAINT FK_kpis_cantonsxdeliverables
+		FOREIGN KEY (kpiid) REFERENCES KPIs(kpiid)
+
 	);
 END
-GO
 
 -- Database Tables Population
 
-IF NOT EXISTS( SELECT id FROM [dbo].[PoliticParties])
+IF NOT EXISTS( SELECT politicpartyid FROM [dbo].[PoliticParties])
 BEGIN
 	INSERT INTO [dbo].[PoliticParties] (name, flagPictureUrl) VALUES 
 		('PAC',
@@ -176,68 +188,150 @@ BEGIN
 		'https://i.ibb.co/tzhKTmM/Bandera-Partido-Restauraci-n-Nacional-Costa-Rica-2022-svg.png')
 END
 
-IF NOT EXISTS( SELECT id FROM [dbo].[CampaignManagers])
+IF NOT EXISTS( SELECT userid FROM [dbo].[Users] )
 BEGIN
-	INSERT INTO [dbo].[CampaignManagers] (bio, pictureUrl, politicPartyId)
+    INSERT INTO [dbo].[Users] (firstname, secondname, lastname, idcard)
+    VALUES
+           --Campaign Manager Users
+        ('Juan', 'Carlos', 'Mora', '154780945'),
+        ('Andrea', 'Pamela', 'Hidalgo', '908740635'),
+           --Campaign Manager and Citizen Users
+        ('Ximena', 'Brillit', 'Newball', '70698244'),
+        ('Jose', 'Maria', 'Alvarez', '985476241'),
+           --Citizens Users
+        ('Dylan', '', 'Gonzalez', '846147846'),
+        ('Luis', 'Diego', 'Aguilar', '367545512'),
+        ('Eliza', 'Angelica', 'Schyler', '146569374'),
+        ('Rodrigo', 'Alonso', 'Trejos', '764319788')
+END
+
+IF NOT EXISTS( SELECT roleid FROM [dbo].[Roles] )
+BEGIN
+    INSERT INTO [dbo].[Roles] (rolename)
+    VALUES
+        ('Citizen'),
+        ('Campaign Manager')
+END
+
+IF NOT EXISTS( SELECT userid FROM [dbo].[UsersXRoles] )
+BEGIN
+    INSERT INTO [dbo].[UsersXRoles] (roleid, userid)
+    VALUES
+           (2, 1),      --Juan CM
+           (2, 2),      --Andrea CM
+           (2, 3),      --Ximena CM  ****
+           (2, 4),      --Jose CM    ****
+           (1, 3),      --Ximena also Citizen  ****
+           (1, 4),      --Jose also Citizen    ****
+           (1, 5),      --Dylan Citizen
+           (1, 6),      --Luis Citizen
+           (1, 7),      --Eliza Citizen
+           (1, 8)       --Rodrigo Citizen
+END
+
+IF NOT EXISTS( SELECT campaignmanagerid FROM [dbo].[CampaignManagers])
+BEGIN
+	INSERT INTO [dbo].[CampaignManagers] (bio, pictureurl, politicpartyid, userid)
 	VALUES 
 		('Doctor en Economia y politica',
 		'https://i.ibb.co/7SkKYsV/PAC.png',
+		1,
 		1),
 		('Doctor en AgroIndustria',
 		'https://i.ibb.co/GQS3FfH/CD-500.png',
+		2,
 		2),
 		('Agricultor',
 		'https://i.ibb.co/84k30Yv/Partido-Liberal-Radical-Autentico-svg.png',
+		3,
 		3),
 		('Licenciado Abogado',
 		'https://i.ibb.co/tzhKTmM/Bandera-Partido-Restauraci-n-Nacional-Costa-Rica-2022-svg.png',
+		4,
 		4)
 END
 
-IF NOT EXISTS( SELECT id FROM [dbo].[Actions])
+IF NOT EXISTS( SELECT userid FROM [dbo].[Citizens])
+BEGIN
+	INSERT INTO [dbo].[Citizens] (userid)
+	VALUES
+		(3), (4), (5), (6), (7), (8)
+END
+
+IF NOT EXISTS( SELECT actionid FROM [dbo].[Actions])
 BEGIN
 	-- PAC
-	INSERT INTO [dbo].[Actions] (politicPartyId, action) VALUES
+	INSERT INTO [dbo].[Actions] (politicPartyId, action)
+	VALUES
 		(1, 'Reparar calles dañadas, restaurar calles en mal estado'),
 		(1, 'Aumentar la seguridad ciudadana'),
 		(1, 'Generar mas empleos'),
+        (1, 'Generar mas donas'),
 		(2, 'Reparar calles dañadas, restaurar calles en mal estado'),
 		(2, 'Eliminar la corrupcion de la politica'),
 		(2, 'Mejora del medio ambiente'),
+	    (2, 'Mejora del tendido electrico'),
 		(3, 'Mejora en la educacion'),
 		(3, 'Implementacion del tren electrico'),
 		(3, 'Incorporacion de Buses biodegradables y electricos'),
+	    (3, 'Inclusividad para personas discapacitadas'),
 		(4, 'Uso de energias renovables al 90% en todo el pais'),
 		(4, 'Acceso a Internet a 90% en todo el pais'),
-		(4, 'Eliminacion de la Analfabetizacion')
-
+		(4, 'Eliminacion de la Analfabetizacion'),
+		(4, 'Eliminacion del phising de internet'),
+        (4, 'Incorporacion del sistema virtual en control de auditorias de impuestos')
 END
 
-IF NOT EXISTS( SELECT id FROM [dbo].[Cantons])
+IF NOT EXISTS( SELECT cantonid FROM [dbo].[Cantons])
 BEGIN
-	INSERT INTO [dbo].[Cantons] (name) VALUES( 'Abangares' )
-	INSERT INTO [dbo].[Cantons] (name) VALUES( 'Acosta (San José, CR)' )
-	INSERT INTO [dbo].[Cantons] (name) VALUES( 'Alajuela (Alajuela, CR)' )
-	INSERT INTO [dbo].[Cantons] (name) VALUES( 'Alajuelita (San José, CR)' )
-	INSERT INTO [dbo].[Cantons] (name) VALUES( 'Alvarado (Cartago, CR)' )
-	INSERT INTO [dbo].[Cantons] (name) VALUES( 'Aserrí (San José, CR)' )
-	INSERT INTO [dbo].[Cantons] (name) VALUES( 'Atenas (Alajuela, CR)' )
-	INSERT INTO [dbo].[Cantons] (name) VALUES( 'Bagaces (Guanacaste, CR)' )
-	INSERT INTO [dbo].[Cantons] (name) VALUES( 'Barva (Heredia, CR)' )
-	INSERT INTO [dbo].[Cantons] (name) VALUES( 'Belén (Heredia, CR)' )
-	INSERT INTO [dbo].[Cantons] (name) VALUES( 'Buenos Aires (Puntarenas, CR)' )
-	INSERT INTO [dbo].[Cantons] (name) VALUES( 'Cañas (Guanacaste, CR)' )
-	INSERT INTO [dbo].[Cantons] (name) VALUES( 'Carrillo (Guanacaste, CR)' )
-	INSERT INTO [dbo].[Cantons] (name) VALUES( 'Cartago (Cartago, CR)' )
-	INSERT INTO [dbo].[Cantons] (name) VALUES( 'Corredores (Puntarenas, CR)' )
-	INSERT INTO [dbo].[Cantons] (name) VALUES( 'Coto Brus (Puntarenas, CR)' )
-	INSERT INTO [dbo].[Cantons] (name) VALUES( 'Curridabat (San José, CR)' )
+	INSERT INTO [dbo].[Cantons] (name)
+	VALUES
+	        ( 'Abangares' ),
+	        ( 'Acosta (San José, CR)' ),
+	        ( 'Alajuela (Alajuela, CR)' ),
+	        ( 'Alajuelita (San José, CR)' ),
+	        ( 'Alvarado (Cartago, CR)' ),
+	        ( 'Aserrí (San José, CR)' ),
+	        ( 'Atenas (Alajuela, CR)' ),
+            ( 'Bagaces (Guanacaste, CR)' ),
+	        ( 'Barva (Heredia, CR)' ),
+	        ( 'Belén (Heredia, CR)' ),
+	        ( 'Buenos Aires (Puntarenas, CR)' ),
+	        ( 'Cañas (Guanacaste, CR)' ),
+	        ( 'Carrillo (Guanacaste, CR)' ),
+	        ( 'Cartago (Cartago, CR)' ),
+	        ( 'Corredores (Puntarenas, CR)' ),
+	        ( 'Coto Brus (Puntarenas, CR)' ),
+	        ( 'Curridabat (San José, CR)' )
 END
+
+IF NOT EXISTS( SELECT citizenid FROM [dbo].[CitizensXCantons])
+BEGIN
+	INSERT INTO [dbo].[CitizensXCantons] (citizenid, cantonid)
+	SELECT citizenid,
+	       (SELECT TOP 1 cantonid FROM Cantons ORDER BY NEWID())
+	FROM Citizens
+END
+
+IF NOT EXISTS( SELECT kpiid FROM [dbo].[KPIs])
+BEGIN
+    INSERT INTO KPIs (kpivalue, measurement)
+    VALUES
+        (20,'Kilometros'),
+        (10, 'Votos de aprobacion'),
+        (5, 'Litros'),
+        (100, 'Porcentaje de poblacion'),
+        (30, 'Horas semanales'),
+        (5, 'Estrellas de satisfaccion'),
+        (67, 'Ganancia neta por dia'),
+        (43, 'Avance semanal')
+END
+
+
 
 -- Random inserts
 
 --Virtual Table to add a bit of reality to random values
-
 DECLARE @LOCAL_DeliverableNames TABLE (name VARCHAR(63))
 
 INSERT INTO @LOCAL_DeliverableNames VALUES
@@ -250,18 +344,6 @@ INSERT INTO @LOCAL_DeliverableNames VALUES
 	('Entregable de evidencias audios'),
 	('Entregable de conclusion de la accion propuesta')
 
-DECLARE @LOCAL_KPI TABLE (kpiValue INT, kpiEntity VARCHAR(63))
-
-INSERT INTO @LOCAL_KPI VALUES
-	(20,'Kilometros'),
-	(10, 'Votos de aprobacion'),
-	(5, 'Litros'),
-	(100, 'Porcentaje de poblacion'),
-	(30, 'Horas semanales'),
-	(5, 'Estrellas de satisfaccion'),
-	(67, 'Ganancia neta por dia'),
-	(43, 'Avance semanal')
-
 DECLARE @quantitypoliticparties INT
 DECLARE @quantityactions INT
 DECLARE @quantitydeliverables INT
@@ -269,12 +351,11 @@ DECLARE @politicpartyid INT
 DECLARE @actionid INT
 DECLARE @cantonid INT
 DECLARE @deliv_name VARCHAR(63)
-DECLARE @kpi_val INT
-DECLARE @kpi_ent VARCHAR(63)
+DECLARE @kpiid INT
 DECLARE @LOCAL_CantonsSupported TABLE (cantonid INT)
 
-SELECT @quantitypoliticparties = COUNT(id) FROM dbo.PoliticParties
-SELECT @quantityactions = COUNT(id) FROM dbo.Actions
+SELECT @quantitypoliticparties = COUNT(politicpartyid) FROM dbo.PoliticParties
+SELECT @quantityactions = COUNT(actionid) FROM dbo.Actions
 SET @politicpartyid = 1
 -- For each politic party
 WHILE @politicpartyid <= @quantitypoliticparties
@@ -283,16 +364,18 @@ BEGIN
 	-- For each Action of that Politic party
 	WHILE @actionid <= @quantityactions
 	BEGIN
-	-- Random between 3 and 7 | x belongs to [3,8[
-		SELECT @quantitydeliverables = RAND()*(8-3)+3;
+	-- Random between 4 and 10 | x belongs to [4,11[
+	--PD: This change has been made due to the 40% deliverables raise requested
+		SELECT @quantitydeliverables = RAND()*(11-4)+4;
 		-- For each Deliverable for each Action for each Politic Party
 		WHILE @quantitydeliverables > 0
 		BEGIN
 			IF RAND() > 0.5 BEGIN
 				-- Pick random id from ids in Cantons table
-				SELECT TOP 1 @cantonid = id FROM dbo.Cantons ORDER BY NEWID()
+				SELECT TOP 1 @cantonid = cantonid FROM dbo.Cantons ORDER BY NEWID()
 			END ELSE BEGIN
-				SELECT @cantonid = RAND()*(33-1)+1; 
+			    -- The 50% of the times it will choose any id from 1 to 11, just to make variation
+				SELECT @cantonid = RAND()*(12-1)+1;
 			END
 
 			-- Insert random deliverable for current Action in current Politic Party
@@ -304,21 +387,22 @@ BEGIN
 					DATEADD(dd, 700*RAND(), '03/07/2022')
 					)
 			-- Insert random deliverable for current Action in current Politic Party
-			SELECT TOP 1 @kpi_val = kpiValue, @kpi_ent = kpiEntity FROM @LOCAL_KPI ORDER BY NEWID()
-			INSERT INTO dbo.CantonsXDeliverables(cantonId, deliverableId, kpiValue, kpiEntity) 
+			SELECT TOP 1 @kpiid = kpiid FROM KPIs ORDER BY NEWID()
+			INSERT INTO dbo.CantonsXDeliverables(cantonId, deliverableId, kpiid)
 			VALUES (@cantonid,
 					IDENT_CURRENT('Deliverables'),
-					@kpi_val,
-					@kpi_ent)
+					@kpiid)
+		    --Raises the deliverables for a canton
 			UPDATE Cantons
 				SET deliverables = deliverables + 1
-			WHERE id = @cantonid
+			WHERE cantonid = @cantonid
+			--Checks if this politicParty is already supporting the current canton
 			IF NOT EXISTS (SELECT cantonid FROM @LOCAL_CantonsSupported as cs
 						WHERE cs.cantonid = @cantonid)
 			BEGIN
 				UPDATE Cantons
 					SET politicPartiesSupport = politicPartiesSupport + 1
-				WHERE id = @cantonid
+				WHERE cantonid = @cantonid
 				INSERT INTO @LOCAL_CantonsSupported(cantonid) VALUES (@cantonid)
 			END
 			SET @quantitydeliverables =  @quantitydeliverables - 1
@@ -328,3 +412,42 @@ BEGIN
 	SET @politicpartyid = @politicpartyid + 1
 	DELETE @LOCAL_CantonsSupported
 END
+
+SELECT * FROM PoliticParties
+SELECT * FROM Users
+SELECT * FROM UsersXRoles
+SELECT * FROM Roles
+SELECT * FROM CampaignManagers
+SELECT * FROM Citizens
+SELECT * FROM CitizensXCantons
+SELECT * FROM Cantons
+SELECT * FROM Actions
+SELECT * FROM Deliverables
+SELECT * FROM CantonsXDeliverables
+SELECT * FROM KPIs
+
+--Sql StoredProcedures
+
+-- Endpoint 1
+
+
+
+-- Endpoint 2
+
+
+
+-- Endpoint 3
+
+
+
+-- Endpoint 4
+
+
+
+-- Endpoint 5
+
+
+
+-- Endpoint 6
+
+
